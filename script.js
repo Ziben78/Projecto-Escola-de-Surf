2d34426b23e6fb7107581f0a855a4482817764b7
Ziben78 <passos.rui@gmail.com>
2023-09-28 11:40:36 +0100

    Alteração da página, sem carrossel

--- script.js
+++ script.js
@@ -1,85 +1,42 @@
-<<<<<<< HEAD
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.querySelector("form");
-  const mensagemEnviada = document.querySelector("#mensagem-enviada");
-  const carouselContainer = document.querySelector('.carousel-container');
-  const images = [
-      "images/SCA - Surf Clube Atlântico (@sc.atlantico) • fotos e vídeos do Instagram_files/image1.jpg",
-      "images/SCA - Surf Clube Atlântico (@sc.atlantico) • fotos e vídeos do Instagram_files/image2.jpg",
-      "images/SCA - Surf Clube Atlântico (@sc.atlantico) • fotos e vídeos do Instagram_files/image3.jpg",
-      // Adicione mais imagens conforme necessário
-  ];
+// script.js
 
   let currentIndex = 0;
 
+  nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateCarousel();
+  });
+
+  prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateCarousel();
+  });
+
   function updateCarousel() {
-      const imageUrl = images[currentIndex];
-      carouselContainer.style.backgroundImage = `url(${imageUrl})`;
-      currentIndex = (currentIndex + 1) % images.length;
+    const imageUrl = images[currentIndex];
+    carouselContainer.style.backgroundImage = `url(${imageUrl})`;
   }
 
   updateCarousel();
 
-  // Alterna as imagens automaticamente a cada 5 segundos
-  setInterval(updateCarousel, 5000);
-
-  form.addEventListener("submit", function (e) {
-      e.preventDefault(); // Evita o envio padrão do formulário
-      // Aqui você pode adicionar lógica de envio de formulário, se necessário
-
-      // Exibe a mensagem de sucesso
-      mensagemEnviada.textContent = "Mensagem enviada!! Entraremos em contato o mais breve possível. Obrigado!";
-      mensagemEnviada.style.display = "block";
-
-      // Limpa o formulário (opcional)
-      form.reset();
-  });
-});
-=======
-const carouselContainer = document.querySelector('.carousel-container');
-const prevButton = document.querySelector('.carousel-prev');
-const nextButton = document.querySelector('.carousel-next');
-const images = document.querySelectorAll('.carousel img');
-
-let currentIndex = 0;
-
-nextButton.addEventListener('click', () => {
-  currentIndex = (currentIndex + 1) % images.length;
-  updateCarousel();
-});
-
-prevButton.addEventListener('click', () => {
-  currentIndex = (currentIndex - 1 + images.length) % images.length;
-  updateCarousel();
-});
-
-function updateCarousel() {
-  const offset = -currentIndex * 100;
-  carouselContainer.style.transform = `translateX(${offset}%)`;
-}
-
-updateCarousel();
-
-// Adicione esta parte apenas se desejar que o carrossel avance automaticamente
-setInterval(() => {
+  // Adicione esta parte para transição automática das imagens a cada 5 segundos
+  setInterval(() => {
     currentIndex = (currentIndex + 1) % images.length;
     updateCarousel();
   }, 5000);
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector("form");
-    const mensagemEnviada = document.querySelector("#mensagem-enviada");
 
-    form.addEventListener("submit", function (e) {
-        e.preventDefault(); // Evita o envio padrão do formulário
-        // Aqui você pode adicionar lógica de envio de formulário, se necessário
+  const form = document.querySelector("form");
+  const mensagemEnviada = document.querySelector("#mensagem-enviada");
 
-        // Exibe a mensagem de sucesso
-        mensagemEnviada.textContent = "Mensagem enviada!! Entraremos em contato o mais breve possível. Obrigado!";
-        mensagemEnviada.style.display = "block";
+  form.addEventListener("submit", function (e) {
+    e.preventDefault(); // Evita o envio padrão do formulário
+    // Aqui você pode adicionar lógica de envio de formulário, se necessário
 
-        // Limpa o formulário (opcional)
-        form.reset();
-    });
-});
+    // Exibe a mensagem de sucesso
+    mensagemEnviada.textContent =
+      "Mensagem enviada!! Entraremos em contato o mais breve possível. Obrigado!";
+    mensagemEnviada.style.display = "block";
 
->>>>>>> 667b8a858bb06b9848fc700fd2a8b1d5d023b3b2
+    // Limpa o formulário (opcional)
+    form.reset();
+  });
